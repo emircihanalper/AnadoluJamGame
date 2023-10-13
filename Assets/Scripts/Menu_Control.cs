3ba45e0e772e67f95e15c0391cc7c656a0492fb1
emircihanalper <emircihanalper@gmail.com>
2023-10-13 19:08:15 +0300

    a

---
+++ Assets/Scripts/Menu_Control.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Control : MonoBehaviour
+{
+    public void Play_button()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Quit_button()
+    {
+        Application.Quit();
+    }
+
+    public void Credits_button()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Settings_button()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Return_button()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
