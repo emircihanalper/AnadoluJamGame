079fdb58965680b58de5dbc05956719b460a769e
Enes Işık <30756694+isikenes@users.noreply.github.com>
2023-10-13 21:50:45 +0300

    a

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,20 +8,33 @@ public class PlayerController : MonoBehaviour
     float input;
     float speed=10f;
     Vector2 targetVelocity;
-    float jumpForce = 15f;
+    float jumpForce = 12f;
     bool isJumped;
     bool isGrounded=true;
     int jumpCount = 0;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         input = Input.GetAxisRaw("Horizontal");
+        if(input>0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if(input<0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        animator.SetBool("isRunning", Mathf.Abs(input) > 0);
+        
         if (Input.GetKeyDown(KeyCode.Space))
         {
             isJumped = true;
@@ -50,6 +63,7 @@ public class PlayerController : MonoBehaviour
             isGrounded = true;
             isJumped = false;
             jumpCount = 0;
+
         }
     }
 
